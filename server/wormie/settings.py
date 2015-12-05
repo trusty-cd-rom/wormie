88a700ac6b118d89eb4578be63fcb6e25473696e
Charles Harrington <charles.harrington@gmail.com>
2015-12-05 15:25:23 -0800

    (setup) create 'worms' app within 'wormie' django project

--- server/wormie/settings.py
+++ server/wormie/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'worms',
 ]
 
 MIDDLEWARE_CLASSES = [
--- server/wormie/urls.py
+++ server/wormie/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('worms.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')),
 ]
