2b45ef4a699299edf72c98949a7b351e6e2229e0
nicolasfujita <nickfujita@gmail.com>
2015-12-08 14:10:19 -0800

    change server

--- orig_client/ios/wormie/AppDelegate.m
+++ orig_client/ios/wormie/AppDelegate.m
@@ -31,7 +31,9 @@
    * on the same Wi-Fi network.
    */
 
+  // jsCodeLocation = [NSURL URLWithString:@"http://10.8.5.90:8081/index.ios.bundle?platform=ios&dev=true"];
   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+
 
   /**
    * OPTION 2
