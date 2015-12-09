5c88bc772443d1257a6fc391fb2b8fac04ae801c
Charles Harrington <charles.harrington@gmail.com>
2015-12-09 11:58:09 -0800

    (fix) adjust serializers, remove permissions from app settings

--- server/wormie/settings.py
+++ server/wormie/settings.py
@@ -56,9 +56,9 @@ REST_FRAMEWORK = {
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
         'rest_framework_social_oauth2.authentication.SocialAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    )
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    # )
 
 }
 
--- server/worms/serializers.py
+++ server/worms/serializers.py
@@ -3,17 +3,6 @@ from worms.models import Wormhole, Submission, Account
 from django.contrib.auth.models import User
 
 
-class WormholeSerializer(serializers.ModelSerializer):
-
-    owner_name = serializers.ReadOnlyField(source='owner.username')
-    submissions = serializers.PrimaryKeyRelatedField(many=True, queryset=Submission.objects.all())
-
-    class Meta:
-        model = Wormhole
-        fields = ('id', 'title', 'latitude', 'longitude',
-            'deadline', 'notes', 'status', 'owner', 'owner_name', 'submissions')
-
-
 class SubmissionSerializer(serializers.ModelSerializer):
 
     owner_name = serializers.ReadOnlyField(source='owner.username')
@@ -23,14 +12,29 @@ class SubmissionSerializer(serializers.ModelSerializer):
         field = ('id', 'notes', 'video_url', 'wormhole_id', 'owner', 'owner_name')
 
 
+class WormholeSerializer(serializers.ModelSerializer):
+
+    # submissions = serializers.PrimaryKeyRelatedField(many=True, queryset=Submission.objects.all())
+    owner_name = serializers.ReadOnlyField(source='owner.username')
+    owner_wormiecolor = serializers.ReadOnlyField(source='owner.wormie_color')
+    submissions = SubmissionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Wormhole
+        fields = ('id', 'title', 'latitude', 'longitude',
+            'deadline', 'notes', 'status', 'owner', 'owner_name', 'owner_wormiecolor', 'submissions')
+
+
 class UserSerializer(serializers.ModelSerializer):
 
-    wormholes = serializers.PrimaryKeyRelatedField(many=True, queryset=Wormhole.objects.all())
-    submissions = serializers.PrimaryKeyRelatedField(many=True, queryset=Submission.objects.all())
+    # wormholes = serializers.PrimaryKeyRelatedField(many=True, queryset=Wormhole.objects.all())
+    # submissions = serializers.PrimaryKeyRelatedField(many=True, queryset=Submission.objects.all())
+    wormholes = WormholeSerializer(many=True, read_only=True)
+    submissions = SubmissionSerializer(many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'id', 'first_name', 'last_name', 'email', 'wormholes', 'submissions')
+        fields = ('username', 'id', 'first_name', 'last_name', 'email', 'wormholes', 'submissions', 'submissions')
 
 
 class AccountSerializer(serializers.ModelSerializer):
