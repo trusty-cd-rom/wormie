63273c464d60ec6522dd3983641d7fa183cf548d
Charles Harrington <charles.harrington@gmail.com>
2015-12-08 16:50:00 -0800

    (feat) facebook auth now creates User and Account, saves avatar_url

--- server/wormie/pipeline.py
+++ server/wormie/pipeline.py
@@ -1,10 +1,9 @@
-# from worms.models import Account, User
-# from worms.serializers import AccountSerializer, UserSerializer
+from worms.models import Account, User
+from worms.serializers import AccountSerializer, UserSerializer
 import logging
 logger = logging.getLogger(__name__)
 
 def create_account(backend, user, response, is_new, *args, **kwargs):
-    logger.info("STARWARS")
     url = "http://graph.facebook.com/" + response["id"] + "/picture?type=large"
-    logger.info(url)
+    AccountSerializer(Account.objects.create(user=user, picture_url=url))
     return None
