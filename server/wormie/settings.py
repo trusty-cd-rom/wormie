c7a0198914af92dc3b7998ce561f21e84500035d
Charles Harrington <charles.harrington@gmail.com>
2015-12-09 15:22:31 -0800

    (fix) adjust static file settings for eb deployment

--- server/wormie/settings.py
+++ server/wormie/settings.py
@@ -245,4 +245,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+
 STATIC_URL = '/static/'
