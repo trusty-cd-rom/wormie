2f1890d220e6ac7ba9122fb4409ebfab9fb3dbc3
Charles Harrington <charles.harrington@gmail.com>
2015-12-08 16:42:54 -0800

    (feat) auth pipeline now logs the facebook avatar url

--- server/wormie/pipeline.py
+++ server/wormie/pipeline.py
@@ -4,5 +4,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 def create_account(backend, user, response, is_new, *args, **kwargs):
-    logger.info("HELLO WORLD")
+    logger.info("STARWARS")
+    url = "http://graph.facebook.com/" + response["id"] + "/picture?type=large"
+    logger.info(url)
     return None
