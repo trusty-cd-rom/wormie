5dccbdbe99082acecc16eb0d360a5af853373007
Charles Harrington <charles.harrington@gmail.com>
2015-12-08 17:33:16 -0800

    (fix) change custom pipeline to get_or_create

--- server/wormie/pipeline.py
+++ server/wormie/pipeline.py
@@ -5,5 +5,5 @@ logger = logging.getLogger(__name__)
 
 def create_account(backend, user, response, is_new, *args, **kwargs):
     url = "http://graph.facebook.com/" + response["id"] + "/picture?type=large"
-    AccountSerializer(Account.objects.create(user=user, picture_url=url))
+    AccountSerializer(Account.objects.get_or_create(user=user, picture_url=url))
     return None
