b14d24987eec0e161e088163074c13d5af48b2fe
Charles Harrington <charles.harrington@gmail.com>
2015-12-09 19:49:00 -0800

    (feat) update serializers to provide more details via the url

--- server/worms/models.py
+++ server/worms/models.py
@@ -12,7 +12,7 @@ class Wormhole(models.Model):
     deadline = models.DateTimeField()
     notes = models.CharField(max_length=2000)
     status = models.CharField(max_length=100)
-    owner = models.ForeignKey(User, related_name='wormholes')
+    requestor = models.ForeignKey(User, related_name='wormholes')
 
     class Meta:
         ordering = ('created_at',)
@@ -23,8 +23,8 @@ class Submission(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     notes = models.TextField(max_length=2000)
     video_url = models.TextField(max_length=255)
-    wormhole_id = models.ForeignKey(Wormhole, related_name='submissions')
-    owner = models.ForeignKey(User, related_name='submissions')
+    wormhole = models.ForeignKey(Wormhole, related_name='submissions')
+    submitter = models.ForeignKey(User, related_name='submissions')
 
     class Meta:
         ordering = ('created_at',)
--- server/worms/serializers.py
+++ server/worms/serializers.py
@@ -3,23 +3,60 @@ from worms.models import Wormhole, Submission, Account
 from django.contrib.auth.models import User
 
 
-class WormholeSerializer(serializers.ModelSerializer):
-
-    submissions = serializers.PrimaryKeyRelatedField(many=True, queryset=Submission.objects.all())
-    # owner_name = serializers.ReadOnlyField(source='owner.username')
-    # owner_wormiecolor = serializers.ReadOnlyField(source='owner.wormie_color')
-    # submissions = SubmissionSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Wormhole
-        fields = ('id', 'title', 'latitude', 'longitude','deadline', 'notes', 'status', 'owner', 'submissions')
-
-
 class SubmissionSerializer(serializers.ModelSerializer):
+
+    # Submitter details
+    submitter_id = serializers.ReadOnlyField(source='submitter.id')
+    submitter_username = serializers.ReadOnlyField(source='submitter.username')
+    submitter_first_name = serializers.ReadOnlyField(source='submitter.first_name')
+    submitter_last_name = serializers.ReadOnlyField(source='submitter.last_name')
+    submitter_picture_url = serializers.ReadOnlyField(source='submitter.account.picture_url')
+    submitter_wormiecolor = serializers.ReadOnlyField(source='submitter.account.wormie_color')
+
+    # Wormhole details
+    wormhole_id = serializers.ReadOnlyField(source='wormhole.id')
+    wormhole_title = serializers.ReadOnlyField(source='wormhole.title')
+    wormhole_latitude = serializers.ReadOnlyField(source='wormhole.latitude')
+    wormhole_longitude = serializers.ReadOnlyField(source='wormhole.longitude')
+    wormhole_deadline = serializers.ReadOnlyField(source='wormhole.deadline')
+    wormhole_notes = serializers.ReadOnlyField(source='wormhole.notes')
+    wormhole_status = serializers.ReadOnlyField(source='wormhole.status')
+    
+    # Requestor details
+    requestor_id = serializers.ReadOnlyField(source='wormhole.requestor_id')
+    requestor_username = serializers.ReadOnlyField(source='wormhole.requestor.username')
+    requestor_first_name = serializers.ReadOnlyField(source='wormhole.requestor.first_name')
+    requestor_last_name = serializers.ReadOnlyField(source='wormhole.requestor.last_name')
+    requestor_picture_url = serializers.ReadOnlyField(source='wormhole.requestor.account.picture_url')
+    requestor_wormiecolor = serializers.ReadOnlyField(source='wormhole.requestor.account.wormie_color')
 
     class Meta:
         model = Submission
-        field = ('id', 'notes', 'video_url', 'wormhole_id', 'owner')
+        field = ('id', 'notes', 'video_url', 'submitter_id', 'submitter_username',
+            'submitter_first_name', 'submitter_last_name', 'submitter_picture_url', 'submitter_wormiecolor',
+            'wormhole_id', 'wormhole_title', 'wormhole_latitude', 'wormhole_longitude', 'wormhole_deadline',
+            'wormhole_notes','wormhole_status', 
+            'requestor_id', 'requestor_username',
+            'requestor_first_name', 'requestor_last_name',
+            'requestor_picture_url', 'requestor_wormiecolor')
+
+class WormholeSerializer(serializers.ModelSerializer):
+
+    # Requestor details
+    requestor_id = serializers.ReadOnlyField(source='requestor.id')
+    requestor_username = serializers.ReadOnlyField(source='requestor.username')
+    requestor_first_name = serializers.ReadOnlyField(source='requestor.first_name')
+    requestor_last_name = serializers.ReadOnlyField(source='requestor.last_name')
+    requestor_picture_url = serializers.ReadOnlyField(source='requestor.account.picture_url')
+    requestor_wormiecolor = serializers.ReadOnlyField(source='requestor.account.wormie_color')
+
+    submissions = SubmissionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Wormhole
+        fields = ('id', 'title', 'latitude', 'longitude','deadline', 'notes', 'status', 'requestor_id', 
+            'requestor_username', 'requestor_first_name', 'requestor_last_name', 
+            'requestor_picture_url', 'requestor_wormiecolor', 'submissions')
 
 
 class AccountSerializer(serializers.ModelSerializer):
@@ -32,17 +69,12 @@ class AccountSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
 
     account = AccountSerializer()
-    
-    # Show only the primary keys for wormholes and submissions in User
-    wormholes = serializers.PrimaryKeyRelatedField(many=True, queryset=Wormhole.objects.all())
-    submissions = serializers.PrimaryKeyRelatedField(many=True, queryset=Submission.objects.all())
-
     # Show all fields for wormholes and submissions in User
-    # wormholes = WormholeSerializer(many=True, read_only=True)
-    # submissions = SubmissionSerializer(many=True, read_only=True)
+    wormholes = WormholeSerializer(many=True, read_only=True)
+    submissions = SubmissionSerializer(many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'account', 'wormholes', 'submissions')
-
-
+        fields = ('id', 'username', 'first_name', 'last_name',
+            'email', 'account', 'wormholes', 'submissions'
+        )
