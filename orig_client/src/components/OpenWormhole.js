1d2221c1b8dbe9cb47833777befcf8a6075d38cd
nicolasfujita <nickfujita@gmail.com>
2015-12-12 16:46:40 -0800

    updated styling for the fulfil request and create new request screens

--- orig_client/src/components/OpenWormhole.js
+++ orig_client/src/components/OpenWormhole.js
@@ -25,43 +25,42 @@ class OpenWormhole extends Component {
       <View style={styles.container}>
 
         <TouchableHighlight
-          style = {styles.loginButton}
+          style = {styles.backButton}
           onPress = {this.back.bind(this)}
-          underlayColor = '#88D4f5'
         >
-          <Text style = {styles.buttonText}> Back </Text>
+          <Text style = {styles.backText}> {'<'} </Text>
         </TouchableHighlight>
+        <View style = {styles.contentContainer}>
+          <Text style={styles.title}>
+            Title
+          </Text>
+          <Text style={styles.text}>
+            {currentWormhole.title}
+          </Text>
 
-        <Text style={styles.title}>
-          Title
-        </Text>
-        <Text style={styles.text}>
-          {currentWormhole.title}
-        </Text>
+          <Text style={styles.title}>
+            Requester
+          </Text>
+          <Text style={styles.text}>
+            {currentWormhole.owner_name}
+          </Text>
 
-        <Text style={styles.title}>
-          Requester
-        </Text>
-        <Text style={styles.text}>
-          {currentWormhole.owner_name}
-        </Text>
+          <Text style={styles.title}>
+            Deadline
+          </Text>
+          <Text style={styles.text}>
+            {currentWormhole.deadline}
+          </Text>
 
-        <Text style={styles.title}>
-          Deadline
-        </Text>
-        <Text style={styles.text}>
-          {currentWormhole.deadline}
-        </Text>
-
-        <Text style={styles.title}>
-          Notes
-        </Text>
-        <Text style={styles.text}>
-          {currentWormhole.notes}
-        </Text>
-        
+          <Text style={styles.title}>
+            Notes
+          </Text>
+          <Text style={styles.text}>
+            {currentWormhole.notes}
+          </Text>
+        </View>
         <TouchableHighlight
-          style = {styles.loginButton}
+          style = {styles.requestButton}
           onPress = {this.startSubmission.bind(this)}
           underlayColor = '#88D4f5'
         >
@@ -75,15 +74,22 @@ class OpenWormhole extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: 'black',
     marginTop: 20
+  },
+  contentContainer: {
+    flex:15,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // backgroundColor: 'black',
+    // marginTop: 20
   },
   text: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 7,
     color: 'white',
   },
   splashImage: {
@@ -99,6 +105,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flex: 1,
     backgroundColor: '#48BBEC'
+  },
+  requestButton: {
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    flex: 2,
+    backgroundColor: '#48BBEC'
+  },
+  backButton: {
+    // flexDirection: 'row',
+    // alignSelf: 'stretch',
+    justifyContent: 'flex-start',
+    flex: 1,
+    backgroundColor: 'black'
+  },
+  backText: {
+    fontSize: 30,
+    color: 'white',
+    alignSelf: 'flex-start',
+    marginLeft: 5
   },
   buttonText: {
     fontSize: 24,
