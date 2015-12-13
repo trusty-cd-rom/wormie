e902898624af23c70fe8e8ca9278f6f76d26b68b
nicolasfujita <nickfujita@gmail.com>
2015-12-12 16:23:43 -0800

    added image for open requests on feed, updated click events on feed

--- orig_client/src/components/Navbar.js
+++ orig_client/src/components/Navbar.js
@@ -30,7 +30,7 @@ class Navbar extends Component{
   render() {
     return (
       <TabBarIOS
-        tintColor="grey"
+        tintColor="#48BBEC"
         barTintColor="black"
         selectedTab={this.state.selectedTab}
       >
