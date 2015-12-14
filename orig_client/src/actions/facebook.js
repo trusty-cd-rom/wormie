74b3c92e38b13b90171afaa4e561532ce03ee131
Charles Harrington <charles.harrington@gmail.com>
2015-12-14 14:49:25 -0800

    (refactor) refactor facebook code again into one function

--- orig_client/src/actions/facebook.js
+++ orig_client/src/actions/facebook.js
@@ -3,37 +3,16 @@ import { CONVERT_FACEBOOK_TOKEN_TO_DJANGO_TOKEN, SET_CURRENT_USER } from '../con
 
 export function convertFacebookToken(tokenData, cb) {
     
-  console.log("I'm in the convertFacebookToken action ");
-  console.log("tokenData: ", tokenData);
   return dispatch => {
     return api.convertToken(tokenData)
       .then((res) => {
-        cb();
-      });
-  }
-};
-
-export function fetchFacebookProfile(cb) {
-
-    console.log("Im in fetchFacebookProfile Action");
-    
-    return dispatch => {
-      
-      return api.fetchFacebookProfileFromFacebook((res) => {
-          console.log("Action page: Result is: ", res);
-          dispatch(getUserDataFromServer(res.id, cb));
-      });
-    }
-};
-
-export function getUserDataFromServer(fb_id, cb) {
-  console.log("getUserDataFromServer: ", fb_id);
-  return dispatch => {
-    return api.getUserDetailsByFacebookID(fb_id)
-      .then((res) => {
-        console.log("Hello");
-        dispatch(setCurrentUser(res));
-        cb();
+        return api.fetchFacebookProfileFromFacebook((res) => {
+            return api.getUserDetailsByFacebookID(res.id)
+              .then((res) => {
+                dispatch(setCurrentUser(res));
+                cb();
+            });
+        });        
       });
   }
 };
--- orig_client/src/components/Facebook.js
+++ orig_client/src/components/Facebook.js
@@ -21,7 +21,6 @@ var {
 class FacebookLogin extends React.Component {
 
   goToSignup() {
-    console.log(this.props);
     this.props.navigator.replace({
       component: Signup
     });
@@ -31,15 +30,8 @@ class FacebookLogin extends React.Component {
 
     var tokenData = "grant_type=convert_token&client_id=LQBBAG7oJGNgdQyFyJg8TgZpNveL3d8PDkVgfgG2&client_secret=FjoMZbsjfuNJPEsuCgGFHTC0ABDh1KhM0odP7yJpDTAVvcrMzxFNSCU0seF6959ekTsCdB0FSbt2deHnHwM8U5GQfKW9WfrDyBlcHyViRxTF6vM0oavydUkByfUBK4HJ&backend=facebook&token=" + token;
 
-    // Use Facebook token to create new user in our database and return OAuth2 token
     this.props.convertFacebookToken(tokenData, () => {
-      // Grab the user's Facebook id from Facebook and look up user in our database
-      console.log("Time to fetchFacebookProfile");
-      this.props.fetchFacebookProfile(() => {
-        // Switch to the next screen after the client knows the user's info
-        console.log("Now let's goToSignup page");
         this.goToSignup();
-      });
     });
   }
 
