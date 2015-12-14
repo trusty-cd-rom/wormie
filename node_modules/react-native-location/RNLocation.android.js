947f9a2c9164ede6a6b947081936fad13cffe268
nicolasfujita <nickfujita@gmail.com>
2015-12-14 10:51:09 -0800

    updated styling

---
+++ node_modules/react-native-location/RNLocation.android.js
@@ -0,0 +1,17 @@
+/**
+ * Stub of RNLocation for Android.
+ *
+ * @providesModule RNLocation
+ * @flow
+ */
+'use strict';
+
+var warning = require('warning');
+
+var RNLocation = {
+  test: function() {
+    warning("Not yet implemented for Android.");
+  }
+};
+
+module.exports = RNLocation;
