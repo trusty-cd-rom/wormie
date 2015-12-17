ca6c0f2dbc7e4502df10ef567e95935d32fd5e66
nicolasfujita <nickfujita@gmail.com>
2015-12-16 22:27:55 -0800

    added bare camera from webrtcmodule to page, stripped out all socket stuff for webrtc connection

---
+++ orig_client/src/components/LiveCamera.js
@@ -0,0 +1,122 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+  TextInput,
+} = React;
+
+var WebRTC = require('react-native-webrtc');
+var {
+  RTCPeerConnection,
+  RTCMediaStream,
+  RTCIceCandidate,
+  RTCSessionDescription,
+  RTCView,
+  RTCSetting,
+} = WebRTC;
+
+var configuration = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
+
+var pcPeers = {};
+var localStream;
+
+function getLocalStream() {
+  console.log('getLocalStream');
+  navigator.getUserMedia({ "audio": true, "video": true }, function (stream) {
+    localStream = stream;
+    container.setState({selfViewSrc: stream.toURL()});
+    container.setState({status: 'ready', info: 'Please enter or create room ID'});
+  }, logError);
+}
+
+function logError(error) {
+  console.log("logError", error);
+}
+
+function mapHash(hash, func) {
+  var array = [];
+  for (var key in hash) {
+    var obj = hash[key];
+    array.push(func(obj, key));
+  }
+  return array;
+}
+
+var container;
+
+var LiveCamera = React.createClass({
+  getInitialState: function() {
+    return {info: 'Initializing', status: 'init', roomID: '', selfViewSrc: null, remoteList: {}};
+  },
+  componentDidMount: function() {
+    container = this;
+    RTCSetting.setAudioOutput('speaker');
+    RTCSetting.setKeepScreenOn(true);
+    RTCSetting.setProximityScreenOff(true);
+    getLocalStream();
+  },
+  _press(event) {
+    this.refs.roomID.blur();
+    this.setState({status: 'connect', info: 'Connecting'});
+    join(this.state.roomID);
+  },
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          {this.state.info}
+        </Text>
+        { this.state.status == 'ready' ?
+          (<View>
+            <TextInput
+              ref='roomID'
+              autoCorrect={false}
+              style={{width: 200, height: 40, borderColor: 'gray', borderWidth: 1}}
+              onChangeText={(text) => this.setState({roomID: text})}
+              value={this.state.roomID}
+            />
+            <TouchableHighlight
+              onPress={this._press}>
+              <Text>Enter room</Text>
+            </TouchableHighlight>
+          </View>) : null
+        }
+        <RTCView streamURL={this.state.selfViewSrc} style={styles.selfView}/>
+        {
+          mapHash(this.state.remoteList, function(remote, index) {
+            return <RTCView key={index} streamURL={remote} style={styles.remoteView}/>
+          })
+        }
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  selfView: {
+    width: 100,
+    height: 100,
+  },
+  remoteView: {
+    width: 100,
+    height: 100,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default LiveCamera;
