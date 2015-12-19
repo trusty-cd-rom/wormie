d5f30c9339ed3e4b6dabea5099d66310d4c478e8
SunyoungKim508 <sunyoungkim508@gmail.com>
2015-12-18 22:02:03 -0800

    (fix) bug in feedlist.js

--- orig_client/src/components/FeedList.js
+++ orig_client/src/components/FeedList.js
@@ -182,18 +182,24 @@ class FeedList extends React.Component{
         </View>
       );
     });
+
+    // <View style = {styles.row}>
+    //             <Image 
+    //               style = {[styles.profilePic, styles.marginLeft]}
+    //               source = {{uri: item.requestor.picture_url}}
+    //             />
+    //             <Text style = {styles.cardRequestor}> {item.requestor.username} </Text>
+    //           </View>
           
     return (
       //use {} for anything that is not html or text. this allows you to run JS in JSX
-      <View>
-        <ScrollView 
-          automaticallyAdjustContentInsets={false}
-          onScroll={() => { console.log('onScroll!'); }}
-          scrollEventThrottle={200}
-          style={styles.container}>
-          {list}
-        </ScrollView>
-      </View>
+      <ScrollView 
+        automaticallyAdjustContentInsets={false}
+        onScroll={() => { console.log('onScroll!'); }}
+        scrollEventThrottle={200}
+        style={styles.container}>
+        {list}
+      </ScrollView>
     );
   }
 };
