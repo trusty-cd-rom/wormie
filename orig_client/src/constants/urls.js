ab473b6741872ac6f1dba70145288ff568f95f05
Charles Harrington <charles.harrington@gmail.com>
2015-12-29 21:08:58 -0800

    (feat) change gm-server to AWS docker container ip address

--- orig_client/src/constants/urls.js
+++ orig_client/src/constants/urls.js
@@ -1,5 +1,6 @@
 const host = 'http://wormie-4-dev.elasticbeanstalk.com/api';
 const authHost = 'http://wormie-4-dev.elasticbeanstalk.com/auth';
+<<<<<<< 7e185d7efc1e42f5780e613469a900f8d19ebd3a
 // const host = 'http://127.0.0.1:8000/api';
 // const authHost = 'http://127.0.0.1:8000/auth';
 
@@ -7,6 +8,9 @@ const authHost = 'http://wormie-4-dev.elasticbeanstalk.com/auth';
 // const host = 'http://localhost:8000/api';
 // const authHost = 'http://localhost:8000/auth';
 const wormieHost = 'http://localhost:7878';
+=======
+const wormieHost = 'http://52.53.249.61';
+>>>>>>> (feat) change gm-server to AWS docker container ip address
 
 const urls = {
 	wormholes: `${host}/wormholes/`,
