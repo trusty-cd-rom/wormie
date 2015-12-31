b2c3b50da899e9bb437ff509560bc25e3f2b8355
SunyoungKim508 <sunyoungkim508@gmail.com>
2015-12-30 21:17:29 -0800

    Ready to polish profile page

--- orig_client/src/components/Facebook.js
+++ orig_client/src/components/Facebook.js
@@ -38,10 +38,10 @@ class FacebookLogin extends React.Component {
   convertToken(token){
 
     // PYTHON(FIX)
-    var tokenData = "grant_type=convert_token&client_id=LQBBAG7oJGNgdQyFyJg8TgZpNveL3d8PDkVgfgG2&client_secret=FjoMZbsjfuNJPEsuCgGFHTC0ABDh1KhM0odP7yJpDTAVvcrMzxFNSCU0seF6959ekTsCdB0FSbt2deHnHwM8U5GQfKW9WfrDyBlcHyViRxTF6vM0oavydUkByfUBK4HJ&backend=facebook&token=" + token;
+    // var tokenData = "grant_type=convert_token&client_id=LQBBAG7oJGNgdQyFyJg8TgZpNveL3d8PDkVgfgG2&client_secret=FjoMZbsjfuNJPEsuCgGFHTC0ABDh1KhM0odP7yJpDTAVvcrMzxFNSCU0seF6959ekTsCdB0FSbt2deHnHwM8U5GQfKW9WfrDyBlcHyViRxTF6vM0oavydUkByfUBK4HJ&backend=facebook&token=" + token;
     
     // Local
-    // var tokenData = "grant_type=convert_token&client_id=gxiQNqWEgSyWT5vXA3yiuU9T5d9y3YK4SrqEnypa&client_secret=0bSCwwYpb3jtDptLligyLL1ZOr1aSAHycxwA5WBEaENbo61mb1Zqg1MJgv3m16vth1j4vEUZnOuXyvJ85pfU2rmCzIkNYzdOODtK0mWphurSlIBs2HlapMhTgEvYJ4aUss&backend=facebook&token=" + token;
+    var tokenData = "grant_type=convert_token&client_id=gxiQNqWEgSyWT5vXA3yiuU9T5d9y3YK4SrqEnypa&client_secret=0bSCwwYpb3jtDptLligyLL1ZOr1aSAHycxwA5WBEaENbo61mb1Zqg1MJgv3m16vth1j4vEUZnOuXyvJ85pfU2rmCzIkNYzdOODtK0mWphurSlIBs2HlapMhTgEvYJ4aUss&backend=facebook&token=" + token;
     
     console.log('FACEBOOK LOGIN');
     this.props.convertFacebookToken(tokenData, () => {
