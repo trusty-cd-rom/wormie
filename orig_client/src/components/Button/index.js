7187d77c600b0b953c54672562e39924db04490f
SunyoungKim508 <sunyoungkim508@gmail.com>
2015-12-30 20:44:03 -0800

    Finish with button

--- orig_client/src/components/Button/index.js
+++
@@ -1,6 +0,0 @@
-import RadioButtons from './radio-buttons';
-import SegmentedControls from './segmented-controls';
-
-export default RadioButtons;
-export { SegmentedControls };
-export { RadioButtons };
